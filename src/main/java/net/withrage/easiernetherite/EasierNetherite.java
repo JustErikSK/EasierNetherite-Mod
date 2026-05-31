c3082167e3056a454afc18593e806d4a09c91708
justerik <sebo.erik@gmail.com>
2026-05-31 22:44:24 +0200

    Initial commit for Fabric 26.1.2 port

---
+++ src/main/java/net/withrage/easiernetherite/EasierNetherite.java
@@ -0,0 +1,24 @@
+package net.withrage.easiernetherite;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
+import net.withrage.easiernetherite.items.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EasierNetherite implements ModInitializer {
+	public static final String MOD_ID = "easiernetherite";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.register();
+		LOGGER.info("Mod EasierNetherite has been loaded!");
+
+		CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register(output -> {
+			output.insertAfter(Items.NETHERITE_SCRAP, ModItems.RAW_NETHERITE);
+		});
+	}
+}
---
+++ src/main/java/net/withrage/easiernetherite/items/ModItems.java
@@ -0,0 +1,33 @@
+package net.withrage.easiernetherite.items;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.withrage.easiernetherite.EasierNetherite;
+import java.util.function.Function;
+
+
+public class ModItems {
+    public static final Item RAW_NETHERITE = registerItem("raw_netherite");
+
+    public static Item registerItem(String name) {
+        return register(name, Item::new, new Item.Properties());
+    }
+
+    private static <T extends Item> T register(String name, Function<Item.Properties, T> factory, Item.Properties properties) {
+        ResourceKey<Item> itemKey = ResourceKey.create(
+                Registries.ITEM,
+                Identifier.fromNamespaceAndPath(EasierNetherite.MOD_ID, name)
+        );
+
+        T item = factory.apply(properties.setId(itemKey));
+        return Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+    }
+
+    public static void register() {
+        EasierNetherite.LOGGER.info("Registering items for Easier Netherite mod...");
+    }
+}
