caeaf68e57b6debb55bf871207892e61ff87ab33
justerik <sebo.erik@gmail.com>
2026-06-13 17:38:52 +0200

    ported v1.1.0 to forge 1.21.1

--- src/main/java/net/withrage/easiernetherite/EasierNetherite.java
+++ src/main/java/net/withrage/easiernetherite/EasierNetherite.java
@@ -28,6 +28,13 @@ public class EasierNetherite {
 		if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
 			event.getEntries().putAfter(
 					new ItemStack(Items.NETHERITE_SCRAP),
+					new ItemStack(ModItems.NETHERITE_NUGGET.get()),
+					CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS
+			);
+		}
+		if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+			event.getEntries().putAfter(
+					new ItemStack(ModItems.NETHERITE_NUGGET.get()),
 					new ItemStack(ModItems.RAW_NETHERITE.get()),
 					CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS
 			);
