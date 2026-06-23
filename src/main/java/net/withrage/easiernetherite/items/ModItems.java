e2fa45b0d2f64f4190afdf1bfa525e8acf3b09aa
justerik <sebo.erik@gmail.com>
2026-06-23 22:58:22 +0200

    Initial commit for Forge 26.2 port

---
+++ src/main/java/net/withrage/easiernetherite/items/ModItems.java
@@ -0,0 +1,30 @@
+package net.withrage.easiernetherite.items;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.withrage.easiernetherite.EasierNetherite;
+
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EasierNetherite.MOD_ID);
+    public static final RegistryObject<Item> RAW_NETHERITE = ITEMS.register("raw_netherite", () -> new Item(new Item.Properties().setId(itemKey("raw_netherite"))));
+    public static final RegistryObject<Item> NETHERITE_NUGGET = ITEMS.register("netherite_nugget", () -> new Item(new Item.Properties().setId(itemKey("netherite_nugget"))));
+
+    private static ResourceKey<Item> itemKey(String name) {
+        return ResourceKey.create(
+                Registries.ITEM,
+                Identifier.fromNamespaceAndPath(EasierNetherite.MOD_ID, name)
+        );
+    }
+
+    public static void register(BusGroup eventBus) {
+        ITEMS.register(eventBus);
+        EasierNetherite.LOGGER.info("Registering items for Easier Netherite mod...");
+    }
+}
